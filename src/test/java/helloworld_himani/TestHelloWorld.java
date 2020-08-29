ecd76f4f150c7fff0e7e19eaf342ef37c6307c09
Himani Pandey <himani.pandey@thoughtspot.com>
2020-08-29 21:20:53 +0530

    First commit

---
+++ src/test/java/helloworld_himani/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld_himani;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+    @Test
+	public void testHelloWorld() {
+		
+		System.out.println(".......Hello world........");
+	}
+
+}
